1c5a783eba7c4148a001a9394429f25f2c4e47af
DanVu <danvu@ya.ru>
2021-02-20 20:39:16 +0300

    added addListener

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,11 +1,23 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import io.qameta.allure.selenide.AllureSelenide;
+import jdk.nashorn.internal.runtime.PropertyListeners;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static jdk.nashorn.internal.runtime.PropertyListeners.addListener;
 
 public class TestBase {
+    static DesiredCapabilities capabilities = new DesiredCapabilities();
+
     @BeforeAll
     static void setup() {
+        addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource());
         Configuration.startMaximized = true;
+        // config for Java + Selenide
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud:4444/wd/hub";
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
     }
 }
